ed757d872da615f056c56d8b186252fcb0ebfa88
Stanko <stanko@bezak.net>
2024-05-18 11:22:10 +0200

    navbar content

--- navbar.tsx
+++ navbar.tsx
@@ -1,13 +1,22 @@
+import { getTeamForUser } from "@db";
+import type { Team } from "@prisma/client";
 import type { SessionRequest } from "@session";
 
-export function navbar(req: SessionRequest) {
+export async function navbar(req: SessionRequest) {
     const user = req.session?.user;
+    let team: Team | null = null;
+    if (user) {
+        team = await getTeamForUser(user.id)
+    }
+
     return (
         <div>
             <nav>
-                <ul>
+                <ul className="flex justify-center">
                     <li><a href={'/user/' + user?.id}>Profil</a></li>
                     <li><a href="/shop">Obchod</a></li>
+                    <li><a href="/transactions">Vykonané transakcie</a></li>
+                    <li>{team?.money}</li>
                 </ul>
             </nav>
         </div>
