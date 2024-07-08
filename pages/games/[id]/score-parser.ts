db5c3f208545d5da733ab4cc2eab75290bd45630
Stanko <stanko@bezak.net>
2024-07-08 13:23:25 +0200

    score parsing

---
+++ pages/games/[id]/score-parser.ts
@@ -0,0 +1,55 @@
+import db from "@db"
+import { getLeaderboardForUser } from "./leaderboard"
+import type { Leaderboard } from "@prisma/client"
+import getConfig from "@config"
+
+function getScore(gameId: string, gameData: Record<string, string>) {
+  switch (gameId) {
+    case 'drift-boss':
+      return JSON.parse(gameData['mjs-drift-boss-game-v1.0.1-dailyreward']).score
+    case 'spect':
+      return JSON.parse(gameData['BestScore'])
+    case 'cookie-clicker':
+      {
+        const save = atob(gameData['cookieCandy'].split('%')[0])
+        console.log(save)
+        return Math.round(parseFloat(save.split('|')[4].split(';')[0]))
+      }
+    default:
+      return 0
+  }
+}
+
+export async function updateLeaderboard(
+  gameId: string,
+  userId: number,
+  gameData: string
+) {
+  let score = getScore(gameId, JSON.parse(gameData))
+  const leaderboard = await getLeaderboardForUser(userId, gameId, score)
+  if (!isBetterScore(score, leaderboard, gameId)) {
+    score = leaderboard.score
+  }
+  await db.leaderboard.update({
+    where: {
+      id: leaderboard.id
+    },
+    data: {
+      score,
+      gameData,
+    }
+  })
+}
+
+
+function isBetterScore(
+  score: number,
+  leaderboard: Leaderboard,
+  gameId: string
+) {
+  if (getConfig().games[gameId].leaderboard.order == 'asc') {
+    return score < leaderboard.score
+  } else {
+    return score > leaderboard.score
+  }
+}
