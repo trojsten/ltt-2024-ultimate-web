bf573e9a78a60fc30cd177dd10a2f6816af7c96f
Oliver Lago <oliver.oli.lago@gmail.com>
2024-08-05 01:26:43 +0200

    Shorten add length to 10 seconds

--- pages/ad/new.tsx
+++ pages/ad/new.tsx
@@ -83,7 +83,9 @@ export async function post(req: SessionRequest): Promise<Response> {
   const contentId = crypto.randomUUID()
   await Bun.write('uploads/' + contentId, formdata.get('content')!)
   const duration =
-    type == 'IMAGE' ? 10 : Math.min(await getDuration('uploads/' + contentId), 30)
+    type == 'IMAGE'
+      ? 10
+      : Math.min(await getDuration('uploads/' + contentId), 10)
   try {
     await buy(
       req.session!.user.id,
