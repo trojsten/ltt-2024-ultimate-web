aa7944ecc2656e96a47fb7d13e798a38a7d9970a
Stanko <stanko@bezak.net>
2024-08-05 03:00:00 +0200

    local CSS

--- main.tsx
+++ main.tsx
@@ -7,10 +7,6 @@ async function getPage(content: JSX.Element, req: SessionRequest) {
     <html>
       <head>
         <link rel="stylesheet" href="/static/app.css" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <script src="/static/navbar.js" defer async />
